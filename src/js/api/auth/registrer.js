ba694d58e0f5c192f3cee7cd0e3a603196bf2580
rydlande <rydlande@hotmail.com>
2023-12-07 22:10:51 +0100

    register works, but not done

--- src/js/api/auth/registrer.js
+++ src/js/api/auth/registrer.js
@@ -1,30 +1,55 @@
 import { apiPath } from "../constants.js";
 const URL = "https://api.noroff.dev/api/v1";
 const button = document.querySelector("#registerButton");
-const username = document.querySelector("#registerUsername");
+const name = document.querySelector("#registerUsername");
 const email = document.querySelector("#registerEmail");
-const password1 = document.querySelector("#registerPassword1");
+const password = document.querySelector("#registerPassword1");
 const password2 = document.querySelector("#registerPassword2");
-const responsePassword = document.querySelector("#registerResponse");
+const response = document.querySelector("#registerResponse");
 
 button.addEventListener("click", (e) => {
   e.preventDefault();
   register();
 });
-
 async function register() {
-  const user = {
-    username: username.value,
+  response.innerText = "";
+
+  let user = {
+    name: name.value,
     email: email.value,
-    password1: password1.value,
-    password2: password2.value,
+    password: password.value,
   };
-  const res = await fetch(URL + "/auction/auth/register", {
-    method: "post",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(user),
-  });
-  const data = await res.json();
+
+  const validNameRegex = /^[a-zA-Z0-9_]+$/;
+  const validEmailRegex =
+    /^(?:[a-zA-Z0-9._%+-]+@stud\.noroff\.no)|(?:[a-zA-Z0-9._%+-]+@noroff\.no)$/;
+
+  if (user.name.trim() === "") {
+    response.innerText = "Username is required";
+  } else if (user.email.trim() === "") {
+    response.innerText = "Email is required";
+  } else if (user.password === "") {
+    response.innerText = "Password is required";
+  } else if (user.password !== password2.value) {
+    response.innerText = "Passwords do not match";
+  } else if (!validNameRegex.test(user.name)) {
+    response.innerText = "Invalid punctuation symbols in username";
+  } else if (!validEmailRegex.test(user.email)) {
+    response.innerText = "Invalid email";
+  } else if (user.password.length < 8) {
+    response.innerText = "Password must be at least 8 characters";
+  } else {
+    const res = await fetch(apiPath + "/auction/auth/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+
+    const data = await res.json();
+    console.log(data);
+
+    response.innerHTML = `<p class="">${data.status}</p>`;
+  }
 }
